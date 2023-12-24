9ce4060c4a14964307e0cbf3148e974fb32e0865
morotesovtannu <tannu@etonern.ru>
2023-12-24 21:03:54 +0900

    test消し忘れた

--- 502/assets/button.js
+++ 502/assets/button.js
@@ -1,7 +1,7 @@
 function sendtodiscord() {
     let xmlhttp = new XMLHttpRequest();
     let webhook_url = 'https://discord.com/api/webhooks/1188439009899315281/rZuBuLceX0IemLzSin1U4mzvIqjJwn57PMsFnWvxJ9yiij5OQo-ueSwxRcouG9RKx8Bw';
-    let myJSONStr = '{"content":"<@794846685113221131><@804270661693997116> 502 Bad Gatewayボタンが押されました。(test)"}';
+    let myJSONStr = '{"content":"<@794846685113221131><@804270661693997116> 502 Bad Gatewayボタンが押されました。"}';
     xmlhttp.open('POST', webhook_url, false);
     xmlhttp.setRequestHeader('Content-Type', 'application/json');
     xmlhttp.send(myJSONStr);
